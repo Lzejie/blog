835c599715120a2af041069b3be4da5dad68373f
[Lzejie] <[L_zejie@163.com]>
2018-01-13 20:17:34 +0800

    [add]newest

--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,16 @@ from datetime import datetime
 from mongoengine import *
 from config import DBNAME, HOST, PORT
 
-connect('test', host=HOST)
+connect(DBNAME, host=HOST)
 
+# 标签类
 class Tags(Document):
     name = StringField(required=True, max_length=20, unique=True)
 
     createdAt = DateTimeField(default=datetime.now())
     updatedAt = DateTimeField(default=datetime.now())
 
+# 评论类，不存在一个具体的表
 class Comment(EmbeddedDocument):
     name = StringField(required=True, max_length=20)
     comment = StringField(required=True, max_length=1000, min_length=5)
@@ -21,19 +23,20 @@ class Comment(EmbeddedDocument):
     request_data = DictField()
     createdAt = StringField(default=datetime.now())
 
+# 文章类
 class Article(Document):
     title = StringField(max_length=50)
     summery = StringField(max_length=300, help_text=u'summery')
     content = StringField()
-    # DENY mean that one tag delete will be stop when an article was exists
+    # DENY 模式表示在Tag被删除时，如果该tag有关联的文本，则会报错，禁止该次删除操作
     tags = ListField(ReferenceField(Tags, reverse_delete_rule=DENY))
     # image_list = ListField(ReferenceField(Image))
     createdAt = DateTimeField(default=datetime.now())
     updatedAt = DateTimeField(default=datetime.now())
 
 if __name__ == '__main__':
-    t = Tags.objects.first()
-    print t.name
+    # t = Tags.objects.first()
+    # print t.name
     tag1 = Tags()
     tag2 = Tags()
     article = Article()
